dac66b6e873db651c648ea8cd33d28ff43cad262
Jay <jayadi.kurniawan@gmail.com>
2018-01-03 04:36:25 +0700

    Initial commit

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,15 +1,17 @@
 import { StackNavigator } from 'react-navigation'
+import DelicyHomeScreen from '../Containers/DelicyHomeScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
+  DelicyHomeScreen: { screen: DelicyHomeScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'LaunchScreen',
+  initialRouteName: 'DelicyHomeScreen',
   navigationOptions: {
     headerStyle: styles.header
   }
